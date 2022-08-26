db8406acd4851f3a60001cc4e5bf95394d0d7646
omriglevi <71283837+omriglevi@users.noreply.github.com>
2022-08-26 10:29:08 +0300

    achieving margins for pacman

--- app/src/main/java/com/example/androidgame/MainActivity.java
+++ app/src/main/java/com/example/androidgame/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView scoreLabel, startLabel ;
     private ImageView pink, black, orange, box ;
 
+    //Size
+    private int frameHeight ;
+    private int boxSize ;
+
     //Positions
     private float boxY ;
 
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Status
     private boolean action_flag = false ;
+    private boolean start_flag = false ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,20 +56,9 @@ public class MainActivity extends AppCompatActivity {
         black.setY(-80.0f);
 
         //Temp
-        startLabel.setVisibility(View.INVISIBLE);
         boxY = 500.0f ;
 
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        changePos() ;
-                    }
-                });
-            }
-        },0, 20);
+
     }
 
     public void changePos(){
@@ -74,16 +69,49 @@ public class MainActivity extends AppCompatActivity {
             //Release
             boxY += 10 ;
         }
+
+        //Keep our pacmen in the main box - set margins for moving
+        if (boxY < 0 ) boxY = 0 ;
+        if (boxY > frameHeight - boxSize) boxY = frameHeight - boxSize ;
         box.setY(boxY);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN){
-            action_flag = true ;
-        }else if (event.getAction() == MotionEvent.ACTION_UP){
-            action_flag = false ;
+        if (!start_flag){
+        start_flag = true ;
+
+        //FrameHeight
+            FrameLayout frameLayout = findViewById(R.id.frame) ;
+            frameHeight = frameLayout.getHeight() ;
+
+            //Box
+            boxY = box.getY() ;
+            boxSize = box.getHeight() ;
+
+
+        startLabel.setVisibility(View.GONE);
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            changePos() ;
+                        }
+                    });
+                }
+            },0, 20);
+
+        } else{
+            if (event.getAction() == MotionEvent.ACTION_DOWN){
+                action_flag = true ;
+            }else if (event.getAction() == MotionEvent.ACTION_UP){
+                action_flag = false ;
+            }
         }
+
         return super.onTouchEvent(event);
     }
 }
